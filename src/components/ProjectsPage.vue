1c07ee2d0a37c8fc11011462b55817236ab871d2
Mihail <mihailcalin.2002@yahoo.com>
2024-02-03 22:30:07 +0200

    initial projects page

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -1,20 +1,53 @@
 <template>
     <div>
-
+        <ul>
+            <li v-for="(project, index) in  projects " :key="index" class="mt-3">
+                <v-card>
+                    <v-card-item>
+                        <v-card-title>
+                            <span>
+                                <h4>{{ project.projectName }}</h4>
+                            </span>
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text style="padding-top: 20px;">
+                        {{ project.projectDescription }}
+                    </v-card-text>
+                </v-card>
+            </li>
+        </ul>
     </div>
 </template>
   
 <script>
+import axios from "axios";
+
 export default {
     name: "ProjectsPage",
     data() {
         return {
-            
+            projects: []
         };
+    }, methods: {
+        getPeriodString(startDate, endDate) {
+            console.log(startDate)
+            console.log(endDate)
+
+            return typeof (startDate) + " - " + endDate.toLocaleString()
+        }
     },
+    mounted() {
+        axios.get('http://localhost:3000/projects')
+            .then(response => {
+                console.log(response)
+                console.log("Am primit raspund pe mounted" + response.data)
+                this.projects = response.data;
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
 };
 </script>
   
-<style scoped>
-
-</style>
+<style scoped></style>
